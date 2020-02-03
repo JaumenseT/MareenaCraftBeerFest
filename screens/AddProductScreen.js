e1090cd938d3add12ee5fbc8c27053133629db3c
ennaco <ennaco2000@gmail.com>
2020-02-03 19:15:32 +0100

    Creando componente y pantalla para añadir productos

---
+++ screens/AddProductScreen.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  Button,
+  StatusBar,
+  Alert,
+  FlatList,
+  TextInput,
+  Image,
+
+  TouchableOpacity,
+
+  
+  
+} from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+import { exportDefaultSpecifier, tsImportEqualsDeclaration } from '@babel/types';
+
+import 'react-native-gesture-handler';
+import { block } from 'react-native-reanimated';
+import AddBeerComponent from '../components/AddBeerComponent'
+
+export default class AddProductScreen extends Component{
+  
+
+    render(){
+      return(
+        <View style={styles.container}> 
+          <AddBeerComponent />
+        </View>
+  
+  
+      )
+    }
+  
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      backgroundColor: '#DBA470',
+      flex:1,
+      alignContent:'center',
+      alignItems:'center'
+      
+    },
+
+  });
