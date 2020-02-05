cdebae565993ddba5875c94705001461a064a80e
josesanchez810 <jose.sanchez.gimenez@gmail.com>
2020-02-05 19:02:25 +0100

    Creado registroscreen y modificado homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,177 +1,269 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, Image } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Image, FlatList } from 'react-native';
 import { ToastAndroid } from 'react-native';
+import 'react-native-gesture-handler';
 
 export default class LoginScreen extends Component {
 
   DB_URL = 'http://localhost:3000';
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: "",
-      password: ""
+    constructor(props) {
+      super(props)
+      this.state ={
+        documentJSON: undefined,
+        datos: [],
+        //name:this.props.navigation.getParam(name, undefined)
+        
+      };
     }
-  }
-
-  login = () => {
-    fetch(this.DB_URL + '/usuarios?userName=' + this.state.user + '&contrasenya=' + this.state.password)
-      .then(resp => resp.json())
-      .then(data => {
-        if (data.length > 0) {
-          this.props.navigation.navigate('Home');
-        } else {
-          ToastAndroid.showWithGravity('Usuario o contraseña incorrectos', ToastAndroid.LONG, ToastAndroid.TOP);
+  
+  UNSAFE_componentWillMount()
+  {
+  
+    fetch('http://localhost:3000/cervezas')
+      .then((resposta) => {
+        if (resposta.ok) {
+          console.log("En willmount");
+          return resposta.json();
+        } 
+        else {
+          console.log("Error conectado con la base de datos");
         }
+      })
+      .then((documentJSON) => this.setState({ datos: documentJSON }))
+      .catch(error => {
+  
+        console.log("Error de red " + error);
+      });
+  
+  
+  
+  }
+  
+  render()
+  {
+    const { navigation } = this.props;
+    if (this.state.datos.length === 0) {
+      return (
+        <View>
+          <Text>
+            Carregant..
+           </Text>
+  
+        </View>
+      );
+    }
+    else {
+      return (
+        <View style = {styles.container}> 
+            
+          <View>
+              <Text style = {styles.textHeader}>
+              Welcome {JSON.stringify(navigation.getParam('user'))}
+              </Text>
+          </View> 
+          <View style = {styles.container}>
+            
+              <FlatList
+              
+              data ={this.state.datos}
+              renderItem = {({item}) => 
+              <View>  
+                  <Text style = {styles.textList}>{"Name: "}{item.nombre}{"\n"}{"Description: "}{item.descripcion}{"\n"} </Text>
+                  <TouchableOpacity style ={styles.boto} onPress={()=> ((this.deleteEle(item.id)))}>
+                      <Text style = {styles.textBoto}>BORRAR</Text>       
+                  </TouchableOpacity>
+                  
+                  <TouchableOpacity style ={styles.boto} onPress={()=> (this.props.navigation.navigate('UpdateElement',{eid:this.state.id}))}>
+                      <Text style = {styles.textBoto}>UPDATE</Text>       
+                  </TouchableOpacity>
+    
+                  <Text>{"------------------------------------------------------------------------------------------------------------"} </Text>
+              </View>}/>
+  
+          </View>
+          <View>
+            <TouchableOpacity style ={styles.boto} onPress={()=> (this.props.navigation.navigate('AddElement'))}>
+                      <Text style = {styles.textBoto}>ADD ELEMENT</Text>       
+            </TouchableOpacity> 
+            
+          </View>
+        </View>
+      );
+        } 
+  }
+  
+  
+  elePost(eid, enom, edescripcio)
+  {
+    var url = 'http://localhost:3000/elements/';
+    var data = {
+      id: eid,
+      nombre: enom,
+      descripcion: edescripcio,
+      
+    };
+  
+    fetch(url, {
+      method: 'POST',
+      credentials: 'same-origin',
+      mode: 'same-origin',
+      body: JSON.stringify(data),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+        
+      }
+    })
+      .then((respuesta) => {
+        if (respuesta.ok) {
+          return respuesta.json();
+        }
+        else {
+          console.log("Error haciendo el POST");
+        }
+      })
+      .then(respuestaJSON => {
+        console.log(respuestaJSON);
+        alert("Post insertado correctamente " + data.userName + " " + data.nom);
+      })
+      .catch(error => {
+  
+        console.log("Error de red " + error);
       });
   }
-
-/*register = () => {
-  this.props.navigation.navigate('Register');
-}*/
-
-render() {
-  return (
-    <View style={styles.container}>
-      <View style={{ flex: 1, alignSelf: 'flex-end', marginRight: 2 }}>
-        <TouchableOpacity
-          style={styles.button_invitado}
-        //onPress={this.register}
-        >
-
-          <Text style={styles.buttonText_register}>
-            Entrar como invitado
-            </Text>
-
-        </TouchableOpacity>
-      </View>
-      <View style={{ flex: 5, alignContent: 'center', justifyContent: 'center', alignItems: 'center' }}>
-        <Image
-          style={styles.imageStyle}
-          source={require('../images/mareenabeer.jpg')}>
-        </Image>
-
-        <TextInput
-          style={styles.textInput}
-          placeholder={"User"}
-          onChangeText={(user) => this.setState({ user })}
-          value={this.state.user}
-        />
-
-        <TextInput
-          style={styles.textInput}
-          placeholder={"Password"}
-          secureTextEntry={true}
-          onChangeText={(password) => this.setState({password})}
-          value={this.state.password}
-        />
-
-        <TouchableOpacity
-          style={styles.button}
-          onPress={this.login}
-        >
-
-          <Text style={styles.buttonText}>
-            Entrar
-        </Text>
-
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={styles.button_register}
-        //onPress={this.register}
-        >
-
-          <Text style={styles.buttonText_register}>
-            o regístrate
-        </Text>
-
-        </TouchableOpacity>
-      </View>
-
-    </View>
-  );
-}
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: "#8cccc3"
-  },
-
-  button: {
-    width: 275,
-    paddingTop: 8,
-    paddingBottom: 8,
-    marginTop: 7,
-    borderRadius: 10,
-    backgroundColor: "#eea37c",
-    borderRightWidth: 5,
-    borderLeftWidth: 5,
-    borderRightColor: '#b56a24',
-    borderLeftColor: '#b56a24',
-  },
-
-  button_register: {
-    width: 275,
-    paddingTop: 8,
-    paddingBottom: 8,
-    borderRightWidth: 5,
-    borderLeftWidth: 5,
-    borderRightColor: '#dba470',
-    borderLeftColor: '#dba470',
-    marginTop: 7,
-    borderRadius: 10,
-    backgroundColor: "#f6e8cb"
-  },
-
-  button_invitado: {
-    width: 275,
-    paddingTop: 8,
-    paddingBottom: 8,
-    borderRightWidth: 5,
-    borderLeftWidth: 5,
-    borderRightColor: '#dba470',
-    borderLeftColor: '#dba470',
-    marginTop: 7,
-    borderRadius: 10,
-    backgroundColor: "#f6e8cb",
-  },
-
-  buttonText: {
-    fontSize: 20,
-    textAlign: 'center',
-    color: 'black',
-    fontWeight: 'bold'
-  },
-
-  buttonText_register: {
-    fontSize: 18,
-    textAlign: 'center',
-    color: 'black',
-    fontWeight: 'bold'
-  },
-
-
-  textInput: {
-    height: 40,
-    borderColor: 'black',
-    borderWidth: 1,
-    width: 250,
-    fontSize: 15,
-    color: 'black',
-    backgroundColor: 'white',
-    borderRadius: 5,
-    margin: 5
-
-  },
-  imageStyle: {
-    width: 240,
-    height: 240,
-    marginBottom: 10,
-    alignSelf: 'center'
+  
+  updateEle(eid, enom, edescripcio)
+  {
+    
+    var url = 'http://localhost:3000/elements/'+eid;
+    var data = {
+      id: eid,
+      nombre: enom,
+      descripcion: edescripcio
+     
+  
+    };
+  
+    fetch(url, {
+      method: 'PUT',
+      credentials: 'same-origin',
+      mode: 'same-origin',
+      body: JSON.stringify(data),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+  
+      .then((respuesta) => {
+        if (respuesta.ok) {
+          return respuesta.json();
+        }
+        else {
+          console.log("Error haciendo el UPDATE");
+        }
+      })
+      .then(respuestaJSON => {
+        console.log(respuestaJSON);
+        alert("Usuario " + data.userName + " actualizado");
+      })
+      .catch(error => {
+        console.log("Error de red " + error);
+      });
+  
+  
   }
-});
+  
+  deleteEle(eid)
+  {
+  
+    var url = 'http://localhost:3000/elements/'+eid;
+  
+  
+    fetch(url, {
+      method: 'DELETE',
+  
+    })
+  
+      .then((respuesta) => {
+        if (respuesta.ok) {
+          return respuesta.json();
+  
+        }
+        else {
+          console.log("Error haciendo el borrado");
+        }
+  
+      })
+      .then(respuestaJSON => {
+        console.log(respuestaJSON);
+        alert("Se ha borrado correctamente");
+  
+      })
+  
+      .catch(error => {
+  
+        console.log("Error de red " + error);
+      });
+  
+    }
+  }
+  
+  
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#DCDCDC',
+    },
+    comptador: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    fonsTot: {
+      flex: 10, 
+    },
+    cap: {
+      flex: 1,
+      borderWidth: 3,
+      borderRadius: 3,
+      backgroundColor:"#ffffff"
+    },
+    llista: {
+      flex: 4,
+      borderWidth: 3,
+      borderRadius: 3,
+      backgroundColor:"#ffffff"
+    },
+    botons: {
+      flex: 2,
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    componentEstil:{
+      flex: 3,
+      backgroundColor:"#ffffff"
+    },
+    boto:{
+      backgroundColor: "#ff0000",
+      borderWidth: 3,
+      padding: 10,
+    },
+    textHeader:{
+      textAlign: "center",
+      fontSize: 40,
+      fontWeight: "bold"
+    },
+    textList:{
+      fontSize: 16,
+      fontWeight: "bold"
+    },
+    
+    
+  });
