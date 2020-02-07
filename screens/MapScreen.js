aeb48098d82254d7c95ed1b6cba173d4f697dab6
tomeksen <tomy_trt@hotmail.com>
2020-02-07 16:48:47 +0100

    subo cambio

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { Text, View, StyleSheet , Dimensions} from 'react-native';
-import Constants from 'expo-constants';
 import MapView,{Marker} from 'react-native-maps';
 
 
@@ -41,7 +40,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
     backgroundColor: '#ecf0f1',
     padding: 8,
   }, 
