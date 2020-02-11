dfd094c4fdc65d3386aefe63fb55f6e093e1c3f6
ennaco <ennaco2000@gmail.com>
2020-02-11 18:30:01 +0100

    Casa Screen cambios

--- screens/CasaScreen.js
+++ screens/CasaScreen.js
@@ -2,73 +2,124 @@ import * as React from 'react';
 import { Text, View, StyleSheet, FlatList,Image,Button, TouchableOpacity,Alert,ScrollView } from 'react-native';
 import { ListItem,Icon } from 'react-native-elements';
 
-const list = [
-  {
-    id:1,
-    title: 'AMBAR',
-    cervezas: [
-      {
-        id:1,
-        name: 'bianca',
-        tipo: 'blanca',
-      },
-      {
-        id:2,
-        name: 'negra',
-        tipo: 'Polla',
-      },
-    ],
-  },
-  {
-    id:2,
-    title: 'Trips',
-    cervezas: [
-      {
-        id:3,
-        name: 'bianca',
-        tipo: 'blanca',
-      },
-      {
-        id:4,
-        name: 'negra',
-        tipo: 'Polla',
-      },
-    ],
-  },
-];
+var list = [];
 
-export default class App extends React.Component {
+export default class CassaScreen extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state={
+      cervezas:[],
+      marcas:[],
+      total:[],
+    }
+  }
+
+  text = (id,l) => {
+    if(id == l.idMarca){
+      return <Text>{l.nom}</Text> 
+    }
+  
+   
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:3000/marcas")
+    .then((response)=> response.json())
+    .then((json) => { console.log(json),this.setState({marcas: json})})
+    .catch((error)=> console.log(error))
+
+  fetch("http://localhost:3000/cervezas")
+    .then((response)=> response.json())
+    .then((json) => { console.log(json),this.setState({cervezas: json})})
+    .catch((error)=> console.log(error))
+
+    this.state.marcas.map((l,i)=>{
+    /*  let cer = []
+      this.state.cervezas.map((z,x)=>{
+        if(z.idMarca == l.id){
+          cer.push(z);
+        }
+
+      })*/
+      list.push({
+        id:l.id,
+        marca: l.marca,
+        cervezas:"h",
+
+      })
+    
+      console.log(list)
+    })
+  }
+
+  listar = () => {
+    list = []
+    this.state.marcas.map((l,i)=>{
+
+      let cer = []
+      this.state.cervezas.map((z,x)=>{
+        if(z.idMarca == l.id){
+          cer.push({
+            id:z.id,
+            idMarca:z.idMarca,
+            nom:z.nom,
+          });
+        }
+
+      })
+      list.push({
+        id:l.id,
+        marca: l.marca,
+        cervezas:cer,
+
+      })
+    
+ 
+    })
+  }
+
   render() {
+    let id;
+    this.listar()
     return (
       <View style={styles.container}>
       <ScrollView>
-        <FlatList
-          data={list}
-          renderItem={({ item }) => (
-            <View style={{flex:1}}>
-            <View style={{backgroundColor:'white',borderBottomWidth:1,flexDirection:'row'}}>
-            <View style={{flex:1}}>
-              <Text style={{alignSelf:'center',fontSize:26,fontWeight:'bold'}}>{item.title}</Text>      
-              </View>
-              </View>
+
+    
               <FlatList 
-              data={item.cervezas}
-              renderItem={({item})=>(
-                <View>
-                <TouchableOpacity onPress={() => console.log('hola')}>
-                <View style={{backgroundColor:'green',borderBottomWidth:1}}>
-                <Text >{item.name}</Text>
-                <Text>{item.tipo}</Text>
-                </View>
-                </TouchableOpacity>
-                </View>
+                data={list}
+                renderItem={({item})=>(
+                  <View>
+                      <TouchableOpacity onPress={() => console.log('hola')}>
+                        <View style={{backgroundColor:'white',borderBottomWidth:1,flex:1}}>
+                          
+                          <Text>{item.marca}</Text> 
+                          <FlatList 
+                            data={item.cervezas}
+                            renderItem={({item})=>(
+                              <View>
+                                <TouchableOpacity onPress={() => console.log('hola')}>
+                              <View style={{backgroundColor:'green',borderBottomWidth:1,flex:1}}>
+                          
+                                <Text>{item.nom}</Text> 
+                          
+
+                            
+                          </View>
+                      </TouchableOpacity>
+                  </View>
               )}
-              keyExtractor={item => item.name}
+         
               />
-            </View>
-          )}
-          keyExtractor={item => item.id}
-        />
+
+                            
+                          </View>
+                      </TouchableOpacity>
+                  </View>
+              )}
+         
+              />
         </ScrollView>
       </View>
       
@@ -83,4 +134,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#ecf0f1',
     padding: 8,
   },
+
+  
 });
